2b05d276e314593ffc9b728dbb9023d9925af599
NagamasPE <nagamas.dinarputra4@gmail.com>
2023-02-16 17:01:49 +0700

    add production page

--- src/pages/Delivery.js
+++ src/pages/Delivery.js
@@ -103,14 +103,6 @@ function Delivery() {
   const [gateStat, setGateStat] = useState(false);
   const [liftStat, setLiftStat] = useState(false);
 
-  const gateHandle = () => {
-    setGateStat(!gateStat);
-  };
-
-  const liftHandle = () => {
-    setLiftStat(!liftStat);
-  };
-
   const handleToggle = (index) => {
     setQtyScan((prevValues) => {
       const newValues = [...prevValues];
@@ -125,6 +117,14 @@ function Delivery() {
       }
       return newValues;
     });
+  };
+
+  const gateHandle = () => {
+    setGateStat(!gateStat);
+  };
+
+  const liftHandle = () => {
+    setLiftStat(!liftStat);
   };
 
   return (
@@ -275,105 +275,6 @@ function Delivery() {
               </div>
             </div>
           </div>
-          {/* <div className={delivery.form}>
-            <div className={delivery.seventy}>
-              <div className={delivery.header}>
-                <div id={delivery.title}>
-                  <div>DN Number</div>
-                  <div>PTV Number</div>
-                  <div>Vendor ID</div>
-                  <div>Car Number/Driver</div>
-                </div>
-                <div id={delivery.value}>
-                  <input type={"text"} placeholder={"Enter Text 1"} />
-                  <input type={"text"} placeholder={"Enter Text 2"} />
-                  <input type={"text"} placeholder={"Enter Text 3"} />
-                  <input type={"text"} placeholder={"Enter Text 4"} />
-                </div>
-              </div>
-            </div>
-            <div className={delivery.thirty}>
-              
-            </div>
-          </div>
-
-          <div className={delivery.form}>
-            <div className={delivery.seventy}>
-              <div className={delivery.input}>
-                <div id={delivery.range}>
-                  <button>1</button>
-                  <button>2</button>
-                  <button>3</button>
-                  <button>4</button>
-                  <button>5</button>
-                  <button>6</button>
-                  <button>7</button>
-                  <button>8</button>
-                </div>
-                <div id={delivery.display}>
-                  <div className={tabs.tabsContainer}>
-                    <div className={tabs.viewTab}>
-                      <table className={tabs.scanTab}>
-                        <thead>
-                          <tr>
-                            <th>Scan</th>
-                            <th>Name</th>
-                            <th>Qty</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {deliveries.map((delivery) => (
-                            <tr key={delivery.scan}>
-                              <td>{delivery.scan}</td>
-                              <td>{delivery.name}</td>
-                              <td>{delivery.qty}</td>
-                            </tr>
-                          ))}
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className={delivery.thirty}>
-              <div id={delivery.gate}>Status Gate</div>
-              <div id={delivery.lifter}>Status Lifter</div>
-            </div>
-          </div>
-
-          <div className={delivery.form}>
-            <div className={delivery.sum}>
-              <div className={tabs.tabsContainer}>
-                <div className={tabs.viewTab}>
-                  <table className="table">
-                    <thead>
-                      <tr>
-                        <th>No</th>
-                        <th>Material Number</th>
-                        <th>Material Description</th>
-                        <th>Package</th>
-                        <th>Quantity</th>
-                        <th>SLOC</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {summeries.map((delivery) => (
-                        <tr key={delivery.name}>
-                          <td>{delivery.no}</td>
-                          <td>{delivery.material}</td>
-                          <td>{delivery.desc}</td>
-                          <td>{delivery.pack}</td>
-                          <td>{delivery.qty}</td>
-                          <td>{delivery.sloc}</td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
     </>
--- src/pages/Production.js
+++ src/pages/Production.js
@@ -1,5 +1,143 @@
+import master from "../components/css/Master.module.css";
+import tabs from "../components/css/Chart.module.css";
+import logo from "./image/logo.png";
+import { useState } from "react";
+import delivery from "./Delivery.module.css";
+
 function Production() {
-  return <></>;
+  const production = [
+    { scan: 1, batch: "Batch A1", name: "Product 1", qty: 5 },
+    { scan: 2, batch: "Batch A1", name: "Product 2", qty: 10 },
+    { scan: 3, batch: "Batch A1", name: "Product 3", qty: 20 },
+    { scan: 4, batch: "Batch A1", name: "Product 4", qty: 20 },
+    { scan: 5, batch: "Batch A1", name: "Product 5", qty: 20 },
+    { scan: 6, batch: "Batch A1", name: "Product 6", qty: 20 },
+    { scan: 7, batch: "Batch A1", name: "Product 7", qty: 20 },
+    { scan: 8, batch: "Batch A1", name: "Product 8", qty: 20 },
+  ];
+
+  const [qtyScan, setQtyScan] = useState(Array(8).fill(false));
+  const [liftStat, setLiftStat] = useState(false);
+
+  const handleToggle = (index) => {
+    setQtyScan((prevValues) => {
+      const newValues = [...prevValues];
+      newValues[index] = !newValues[index];
+      // Set all other values to false if the current value is true
+      if (newValues[index]) {
+        for (let i = 0; i < newValues.length; i++) {
+          if (i !== index) {
+            newValues[i] = false;
+          }
+        }
+      }
+      return newValues;
+    });
+  };
+
+  const liftHandle = () => {
+    setLiftStat(!liftStat);
+  };
+
+  return (
+    <>
+      <div className={master.main}>
+        <div className={master.container}>
+          <h2>Production System</h2>
+          <div className={delivery.newContainer}>
+            <div className={delivery.content}>
+              <div className={delivery.hForm}>
+                <div className={delivery.full}>
+                  <div id={delivery.menu}>
+                    <button style={{ backgroundColor: "yellow" }}>
+                      CASTING
+                    </button>
+                    <button style={{ backgroundColor: "lightsalmon" }}>
+                      FINISHING/DRILLING/T6
+                    </button>
+                    <button style={{ backgroundColor: "lightgreen" }}>
+                      MACHINING
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div className={delivery.hForm}>
+                <div className={delivery.full}>
+                  <div id={delivery.range}>
+                    <div>QTY:</div>
+                    {qtyScan.map((value, index) => (
+                      <div
+                        className={
+                          `${delivery.rowButton} ` +
+                          (value ? `${delivery.buttonOn}` : "")
+                        }
+                      >
+                        <button key={index} onClick={() => handleToggle(index)}>
+                          {index + 1}
+                        </button>
+                      </div>
+                    ))}
+                  </div>
+                  <div id={delivery.display}>
+                    <div className={tabs.tabsContainer}>
+                      <div className={tabs.viewTab}>
+                        <table className={tabs.scanTab}>
+                          <thead>
+                            <tr>
+                              <th>Scan</th>
+                              <th>No Batch Production</th>
+                              <th>Material Number</th>
+                              <th>Qty</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {production.map((product) => (
+                              <tr key={delivery.scan}>
+                                <td>{product.scan}</td>
+                                <td>{product.batch}</td>
+                                <td>{product.name}</td>
+                                <td>{product.qty}</td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className={delivery.side}>
+              <div className={delivery.vForm}>
+                <div className={delivery.logo}>
+                  <img alt="logo astra" src={logo} />
+                </div>
+                <div className={delivery.buttons}>
+                  <div className={delivery.sidepane}>
+                    <button>INTERLOCK BYPASS</button>
+                    <div className={delivery.stat}>
+                      <div style={{ fontWeight: "bold" }}>Status Lifter</div>
+                      <div
+                        onClick={() => liftHandle()}
+                        className={
+                          `${delivery.indicator} ` +
+                          (liftStat ? `${delivery.statOn}` : "")
+                        }
+                      >
+                        {liftStat ? "ON" : "OFF"}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Production;
