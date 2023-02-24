4735dd1b77caadb20613f2c38ef1de43b9cb823a
NagamasPE <nagamas.dinarputra4@gmail.com>
2023-02-24 16:44:58 +0700

    edit print header layout

--- src/components/layout/PageToPrint.js
+++ src/components/layout/PageToPrint.js
@@ -43,191 +43,170 @@ function PageToPrint(props) {
     <>
       <div className={print.displayContainer}>
         <div ref={componentRef} className={print.printContainer}>
-          <table className={print.header}>
-            <thead>
-              <tr>
-                <td>
-                  <div>
-                    <div className={print.row}>
-                      <div id={print.col1}>
-                        <div>PT. Astra Otopart TBK</div>
-                        <div>Nusametal Division</div>
-                      </div>
-                      <div id={print.barcode}>
-                        <div>PO/PPIC/PPC/001 R:0 ms_edge</div>
-                        <div>
-                          <Barcode
-                            value="(21)21667788"
-                            format="CODE128"
-                            width={1.5}
-                            height={30}
-                            displayValue={false}
-                            margin={0}
-                          />
-                          {/* <QrGenerator value="(21)21667788" /> */}
-                        </div>
-                      </div>
-                    </div>
-                    <div className={print.row}>
-                      <div className={print.docTitle}>
-                        Delivery Note Outgoing Subcontracting
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div>
-                    <div className={print.row}>
-                      <div id={print.col1}>
-                        <div>
-                          <div id={print.title}>DN Number</div>
-                          <div id={print.value}>12345</div>
-                        </div>
-                        {/* <div>
-                          <div id={print.title}>PTV Number</div>
-                          <div id={print.value}>12345</div>
-                        </div>
-                        <div>
-                          <div id={print.title}>Plant</div>
-                          <div id={print.value}>12345</div>
-                        </div>
-                        <div>
-                          <div id={print.title}>Vendor</div>
-                          <div id={print.value}>12345</div>
-                        </div> */}
-                      </div>
-                      <div id={print.col2}>
-                        <div>
-                          <div id={print.title}>Storage Loc</div>
-                          <div id={print.value}>12345</div>
-                        </div>
-                        {/* <div>
-                          <div id={print.title}>Delivery Date</div>
-                          <div id={print.value}>12345</div>
-                        </div>
-                        <div>
-                          <div id={print.title}>Print By</div>
-                          <div id={print.value}>12345</div>
-                        </div> */}
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </thead>
+          
+          {setCreateNote && <div className={print.kop}>
+            <div className={print.row}>
+              <div id={print.col1}>
+                <div>PT. Astra Otopart TBK</div>
+                <div>Nusametal Division</div>
+              </div>
+              <div id={print.barcode}>
+                <div id={print.formDetail}>PO/PPIC/PPC/001 R:0 ms_edge</div>
+                <div>
+                  <Barcode
+                    value="(21)21667788"
+                    format="CODE128"
+                    width={1.5}
+                    height={30}
+                    displayValue={false}
+                    margin={0}
+                  />
+                  {/* <QrGenerator value="(21)21667788" /> */}
+                </div>
+              </div>
+            </div>
+            <div className={print.row}>
+              <div className={print.docTitle}>
+                Delivery Note Outgoing Subcontracting
+              </div>
+            </div>
 
-            <tbody>
-              <tr>
-                <td>
-                  <div className={print.row}>
-                    <table className={print.summeries}>
+            <div className={print.row}>
+              <div id={print.col1}>
+                <div>
+                  <div id={print.title}>DN Number</div>
+                  <div id={print.value}>12345</div>
+                </div>
+                <div>
+                  <div id={print.title}>PTV Number</div>
+                  <div id={print.value}>12345</div>
+                </div>
+                <div>
+                  <div id={print.title}>Plant</div>
+                  <div id={print.value}>12345</div>
+                </div>
+                <div>
+                  <div id={print.title}>Vendor</div>
+                  <div id={print.value}>12345</div>
+                </div>
+              </div>
+              <div id={print.col2}>
+                <div>
+                  <div id={print.title}>Storage Loc</div>
+                  <div id={print.value}>12345</div>
+                </div>
+                <div>
+                  <div id={print.title}>Delivery Date</div>
+                  <div id={print.value}>12345</div>
+                </div>
+                <div>
+                  <div id={print.title}>Print By</div>
+                  <div id={print.value}>12345</div>
+                </div>
+              </div>
+            </div>
+          </div>}
+
+          <div className={print.isi}>
+            <div className={print.row}>
+              <table className={print.summeries}>
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Material Number</th>
+                    <th>
+                      <div>Material Description</div>
+                    </th>
+                    <th>Package</th>
+                    <th>Quantity</th>
+                    <th>SLOC</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {summeries.map((delivery) => (
+                    <tr key={delivery.name}>
+                      <td>{delivery.no}</td>
+                      <td>{delivery.material}</td>
+                      <td>{delivery.desc}</td>
+                      <td>{delivery.pack}</td>
+                      <td>{delivery.qty}</td>
+                      <td>{delivery.sloc}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div className={print.isi}>
+            <div className={print.row} style={{ textTransform: "uppercase" }}>
+              <div style={{ fontWeight: "bold" }}>Driver:</div>
+              <div>W 3024 UN</div>
+            </div>
+          </div>
+
+          {/*<div className={print.row}>
+                    <table
+                      style={{
+                        width: "100%",
+                        border: "1px solid",
+                        textAlign: "center",
+                      }}
+                    >
                       <thead>
                         <tr>
-                          <th>No</th>
-                          <th>Material Number</th>
                           <th>
-                            <div>Material Description</div>
+                            <div>Gambar</div>
+                            <div>Detail</div>
                           </th>
-                          <th>Package</th>
-                          <th>Quantity</th>
-                          {/* <th>SLOC</th> */}
                         </tr>
                       </thead>
+
                       <tbody>
-                        {summeries.map((delivery) => (
-                          <tr key={delivery.name}>
-                            <td>{delivery.no}</td>
-                            <td>{delivery.material}</td>
-                            <td>{delivery.desc}</td>
-                            <td>{delivery.pack}</td>
-                            <td>{delivery.qty}</td>
-                            {/* <td>{delivery.sloc}</td> */}
+                        {Table_data.val.map((val) => (
+                          <tr>
+                            <td>
+                              <BufferToImg
+                                buffer={val.gambar}
+                                key={val.index}
+                              />
+                            </td>
                           </tr>
                         ))}
                       </tbody>
                     </table>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div
-                    className={print.row}
-                    style={{ textTransform: "uppercase" }}
-                  >
-                    <div style={{ fontWeight: "bold" }}>Driver:</div>
-                    <div>W 3024 UN</div>
-                  </div>
-                </td>
-              </tr>
+                  </div>*/}
 
-              {/* <div className={print.row}>
-                      <table
-                        style={{
-                          width: "100%",
-                          border: "1px solid",
-                          textAlign: "center",
-                        }}
-                      >
-                        <thead>
-                          <tr>
-                            <th>
-                              <div>Gambar</div>
-                              <div>Detail</div>
-                            </th>
-                          </tr>
-                        </thead>
-
-                        <tbody>
-                          {Table_data.val.map((val) => (
-                            <tr>
-                              <td>
-                                <BufferToImg
-                                  buffer={val.gambar}
-                                  key={val.index}
-                                />
-                              </td>
-                            </tr>
-                          ))}
-                        </tbody>
-                      </table>
-                    </div> */}
-              <tr>
-                <td>
-                  <div className={print.row}>
-                    <div id={print.sign}>
-                      <div>
-                        <div>Vendor</div>
-                        <div id={print.signName}>
-                          {
-                            "(........................................................)"
-                          }
-                        </div>
-                      </div>
-                      <div>
-                        <div>Approved By</div>
-                        <div id={print.signName}>
-                          {
-                            "(........................................................)"
-                          }
-                        </div>
-                      </div>
-                      <div>
-                        <div>Warehouse</div>
-                        <div id={print.signName}>
-                          {
-                            "(........................................................)"
-                          }
-                        </div>
-                      </div>
-                    </div>
+          <div className={print.isi}>
+            <div className={print.row}>
+              <div id={print.sign}>
+                <div>
+                  <div>Vendor</div>
+                  <div id={print.signName}>
+                    {
+                      "(........................................................)"
+                    }
                   </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                </div>
+                <div>
+                  <div>Approved By</div>
+                  <div id={print.signName}>
+                    {
+                      "(........................................................)"
+                    }
+                  </div>
+                </div>
+                <div>
+                  <div>Warehouse</div>
+                  <div id={print.signName}>
+                    {
+                      "(........................................................)"
+                    }
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <br />
