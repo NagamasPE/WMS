5c34943f06c5051469d6a738787661934f491b01
NagamasPE <nagamas.dinarputra4@gmail.com>
2023-02-27 09:34:17 +0700

    improve header page printing

--- src/components/layout/PageToPrint.js
+++ src/components/layout/PageToPrint.js
@@ -43,69 +43,70 @@ function PageToPrint(props) {
     <>
       <div className={print.displayContainer}>
         <div ref={componentRef} className={print.printContainer}>
-          
-          {setCreateNote && <div className={print.kop}>
-            <div className={print.row}>
-              <div id={print.col1}>
-                <div>PT. Astra Otopart TBK</div>
-                <div>Nusametal Division</div>
-              </div>
-              <div id={print.barcode}>
-                <div id={print.formDetail}>PO/PPIC/PPC/001 R:0 ms_edge</div>
-                <div>
-                  <Barcode
-                    value="(21)21667788"
-                    format="CODE128"
-                    width={1.5}
-                    height={30}
-                    displayValue={false}
-                    margin={0}
-                  />
-                  {/* <QrGenerator value="(21)21667788" /> */}
+          {setCreateNote && (
+            <div className={print.kop}>
+              <div className={print.row}>
+                <div id={print.col1}>
+                  <div>PT. Astra Otopart TBK</div>
+                  <div>Nusametal Division</div>
+                </div>
+                <div id={print.barcode}>
+                  <div id={print.formDetail}>PO/PPIC/PPC/001 R:0 ms_edge</div>
+                  <div>
+                    <Barcode
+                      value="(21)21667788"
+                      format="CODE128"
+                      width={1.5}
+                      height={30}
+                      displayValue={false}
+                      margin={0}
+                    />
+                    {/* <QrGenerator value="(21)21667788" /> */}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className={print.row}>
-              <div className={print.docTitle}>
-                Delivery Note Outgoing Subcontracting
+              <div className={print.row}>
+                <div className={print.docTitle}>
+                  Delivery Note Outgoing Subcontracting
+                </div>
               </div>
-            </div>
 
-            <div className={print.row}>
-              <div id={print.col1}>
-                <div>
-                  <div id={print.title}>DN Number</div>
-                  <div id={print.value}>12345</div>
+              <div className={print.row}>
+                <div id={print.col1}>
+                  <div>
+                    <div id={print.title}>DN Number</div>
+                    <div id={print.value}>12345</div>
+                  </div>
+                  <div>
+                    <div id={print.title}>PTV Number</div>
+                    <div id={print.value}>12345</div>
+                  </div>
+                  <div>
+                    <div id={print.title}>Plant</div>
+                    <div id={print.value}>12345</div>
+                  </div>
+                  <div>
+                    <div id={print.title}>Vendor</div>
+                    <div id={print.value}>12345</div>
+                  </div>
                 </div>
-                <div>
-                  <div id={print.title}>PTV Number</div>
-                  <div id={print.value}>12345</div>
-                </div>
-                <div>
-                  <div id={print.title}>Plant</div>
-                  <div id={print.value}>12345</div>
-                </div>
-                <div>
-                  <div id={print.title}>Vendor</div>
-                  <div id={print.value}>12345</div>
-                </div>
-              </div>
-              <div id={print.col2}>
-                <div>
-                  <div id={print.title}>Storage Loc</div>
-                  <div id={print.value}>12345</div>
-                </div>
-                <div>
-                  <div id={print.title}>Delivery Date</div>
-                  <div id={print.value}>12345</div>
-                </div>
-                <div>
-                  <div id={print.title}>Print By</div>
-                  <div id={print.value}>12345</div>
+                <div id={print.col2}>
+                  <div>
+                    <div id={print.title}>Storage Loc</div>
+                    <div id={print.value}>12345</div>
+                  </div>
+                  <div>
+                    <div id={print.title}>Delivery Date</div>
+                    <div id={print.value}>12345</div>
+                  </div>
+                  <div>
+                    <div id={print.title}>Print By</div>
+                    <div id={print.value}>12345</div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>}
+          )}
 
           <div className={print.isi}>
             <div className={print.row}>
@@ -123,7 +124,7 @@ function PageToPrint(props) {
                   </tr>
                 </thead>
                 <tbody>
-                  {summeries.map((delivery) => (
+                  {summeries.slice(1, 14).map((delivery, index) => (
                     <tr key={delivery.name}>
                       <td>{delivery.no}</td>
                       <td>{delivery.material}</td>
@@ -133,15 +134,49 @@ function PageToPrint(props) {
                       <td>{delivery.sloc}</td>
                     </tr>
                   ))}
+                  {summeries.slice(1, 14).length === 0 && (
+                    <tr>
+                      <td colSpan="6">No data found</td>
+                    </tr>
+                  )}
                 </tbody>
               </table>
             </div>
           </div>
 
           <div className={print.isi}>
-            <div className={print.row} style={{ textTransform: "uppercase" }}>
-              <div style={{ fontWeight: "bold" }}>Driver:</div>
-              <div>W 3024 UN</div>
+            <div className={print.row}>
+              <table className={print.summeries}>
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Material Number</th>
+                    <th>
+                      <div>Material Description</div>
+                    </th>
+                    <th>Package</th>
+                    <th>Quantity</th>
+                    <th>SLOC</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {summeries.slice(15, 28).map((delivery, index) => (
+                    <tr key={delivery.name}>
+                      <td>{delivery.no}</td>
+                      <td>{delivery.material}</td>
+                      <td>{delivery.desc}</td>
+                      <td>{delivery.pack}</td>
+                      <td>{delivery.qty}</td>
+                      <td>{delivery.sloc}</td>
+                    </tr>
+                  ))}
+                  {summeries.slice(15, 28).length === 0 && (
+                    <tr>
+                      <td colSpan="6">No data found</td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
             </div>
           </div>
 
@@ -178,6 +213,11 @@ function PageToPrint(props) {
                   </div>*/}
 
           <div className={print.isi}>
+            <div className={print.row} style={{ textTransform: "uppercase" }}>
+              <div style={{ fontWeight: "bold" }}>Driver:</div>
+              <div>W 3024 UN</div>
+            </div>
+
             <div className={print.row}>
               <div id={print.sign}>
                 <div>
